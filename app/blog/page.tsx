7de50336fb84f99cb3d85a07bbe900336c7dd3eb
TKMhub <111867865+TKMhub@users.noreply.github.com>
2025-06-12 23:13:02 +0900

    0612_tags

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,12 +6,12 @@ import { BlogGrid } from "../components/blog/BlogGrid";
 export default async function HomePage() {
   const posts: BlogPost[] = await getPublishedArticles();
 
-  // Notion → BlogGrid 用の型に変換（author/日付などは仮）
   const blogs = posts.map((post) => ({
     title: post.title,
     date: post.publishedAt,
     author: post.author,
     imageUrl: undefined,
+    tags: post.tags,
   }));
 
   return (
