61f6ef7fed05a642cedf10c348c50ad8946355d7
TKMhub <111867865+TKMhub@users.noreply.github.com>
2025-08-01 23:17:27 +0900

    fix

--- app/products/layout.tsx
+++ app/products/layout.tsx
@@ -1,18 +1,18 @@
-import type { Metadata } from 'next'
-import { Header } from '@/components/navigation/Header'
-import { Footer } from '@/app/components/Footer'
+import type { Metadata } from "next";
+import { HeaderProduct } from "../components/HeaderProduct";
+import { Footer } from "../components/Footer";
 
 export const metadata: Metadata = {
-  title: 'Products',
-  description: 'Product distribution',
-}
+  title: "Products",
+  description: "Product distribution",
+};
 
-export default function ProductsLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
+      <HeaderProduct />
       <main className="flex-grow mt-[130px]">{children}</main>
       <Footer />
     </div>
-  )
+  );
 }
